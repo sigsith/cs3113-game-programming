68d1325059cce1c219d1e9853296fcc738c42007
yifei-yao <yaoyifei0@gmail.com>
2023-06-10 18:08:36 -0400

    Create project

---
+++ simple-2d-scene/simple-2d-scene/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  simple-2d-scene
+//
+//  Created by Yifei Yao on 6/10/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}

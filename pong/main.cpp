f8db8c15a76d83439254af4f6ce6cd022f4b1867
yifei-yao <yaoyifei0@gmail.com>
2023-06-25 16:06:22 -0400

    Fix bad code

--- pong/main.cpp
+++ pong/main.cpp
@@ -1,6 +1,16 @@
+/**
+* Author: Yifei Yao
+* Assignment: Pong Clone
+* Date due: 2023-06-25, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
+// -------- SECTION FORBID CHANGE --------
+// Contract code such as header files, shader path.
 #define GL_SILENCE_DEPRECATION
 #define GL_GLEXT_PROTOTYPES 1
-#define LOG(argument) std::cout << argument << '\n'
 
 #ifdef _WINDOWS
 #include <GL/glew.h>
@@ -12,101 +22,89 @@
 #include "glm/gtc/matrix_transform.hpp"
 #include "ShaderProgram.h"
 
-const int WINDOW_WIDTH  = 640,
-          WINDOW_HEIGHT = 480;
+constexpr char V_SHADER_PATH[] = "shaders/vertex.glsl",
+        F_SHADER_PATH[] = "shaders/fragment.glsl";
 
-const float BG_RED     = 0.1922f,
-            BG_BLUE    = 0.549f,
-            BG_GREEN   = 0.9059f,
-            BG_OPACITY = 1.0f;
+// -------- SECTION GLOBAL CONSTANTS AND DEFINITIONS --------
+// Compile time configurations.
+constexpr int WINDOW_WIDTH = 640,
+        WINDOW_HEIGHT = 480;
 
-const int VIEWPORT_X      = 0,
-          VIEWPORT_Y      = 0,
-          VIEWPORT_WIDTH  = WINDOW_WIDTH,
-          VIEWPORT_HEIGHT = WINDOW_HEIGHT;
+constexpr float BG_RED = 0.1922f,
+        BG_BLUE = 0.549f,
+        BG_GREEN = 0.9059f,
+        BG_OPACITY = 1.0f;
 
-const char V_SHADER_PATH[] = "shaders/vertex.glsl",
-           F_SHADER_PATH[] = "shaders/fragment.glsl";
+constexpr int VIEWPORT_X = 0,
+        VIEWPORT_Y = 0,
+        VIEWPORT_WIDTH = WINDOW_WIDTH,
+        VIEWPORT_HEIGHT = WINDOW_HEIGHT;
 
-const int TRIANGLE_RED     = 1.0,
-          TRIANGLE_BLUE    = 0.4,
-          TRIANGLE_GREEN   = 0.4,
-          TRIANGLE_OPACITY = 1.0;
+constexpr double TRIANGLE_RED = 1.0,
+        TRIANGLE_BLUE = 0.4,
+        TRIANGLE_GREEN = 0.4,
+        TRIANGLE_OPACITY = 1.0;
 
-const float MILLISECONDS_IN_SECOND = 1000.0;
-const float DEGREES_PER_SECOND     = 90.0f;
+constexpr float MILLISECONDS_IN_SECOND = 1000.0;
 
-SDL_Window* g_display_window;
-
+// -------- SECTION GLOBAL VARIABLES --------
+// Game level variable states; to be extensively shared.
+// Keep this as small as possible.
+SDL_Window *g_display_window;
 bool g_game_is_running = true;
-bool g_is_growing      = true;
+ShaderProgram program;
 
-ShaderProgram g_program;
-glm::mat4 g_view_matrix,
-          g_model_matrix,
-          g_projection_matrix,
-          g_trans_matrix;
-
-float g_triangle_x      = 0.0f;
-float g_triangle_rotate = 0.0f;
-float g_previous_ticks  = 0.0f;
-
-void initialise()
-{
+// -------- SECTION FUNCTIONS --------
+void initialise() {
     SDL_Init(SDL_INIT_VIDEO);
-    g_display_window = SDL_CreateWindow("Hello, Delta Time!",
-                                        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+    g_display_window = SDL_CreateWindow("Hello, Pong!",
+                                        SDL_WINDOWPOS_CENTERED,
+                                        SDL_WINDOWPOS_CENTERED,
                                         WINDOW_WIDTH, WINDOW_HEIGHT,
                                         SDL_WINDOW_OPENGL);
-    
+
     SDL_GLContext context = SDL_GL_CreateContext(g_display_window);
     SDL_GL_MakeCurrent(g_display_window, context);
-    
+
 #ifdef _WINDOWS
     glewInit();
 #endif
-    
     glViewport(VIEWPORT_X, VIEWPORT_Y, VIEWPORT_WIDTH, VIEWPORT_HEIGHT);
-    
-    g_program.Load(V_SHADER_PATH, F_SHADER_PATH);
-    
-    g_view_matrix = glm::mat4(1.0f);  // Defines the position (location and orientation) of the camera
-    g_projection_matrix = glm::ortho(-5.0f, 5.0f, -3.75f, 3.75f, -1.0f, 1.0f);  // Defines the characteristics of your camera, such as clip planes, field of view, projection method etc.
-    g_trans_matrix = g_model_matrix;
-    
-    g_program.SetProjectionMatrix(g_projection_matrix);
-    g_program.SetViewMatrix(g_view_matrix);
+    program.Load(V_SHADER_PATH, F_SHADER_PATH);
+    auto view_matrix = glm::mat4(
+            1.0f);  // Defines the position (location and orientation) of the camera
+    const auto projection_matrix = glm::ortho(-5.0f, 5.0f, -3.75f, 3.75f,
+                                              -1.0f,
+                                              1.0f);  // Defines the characteristics of your camera, such as clip planes, field of view, projection method etc.
+    program.SetProjectionMatrix(projection_matrix);
+    program.SetViewMatrix(view_matrix);
     // Notice we haven't set our model matrix yet!
-    
-    g_program.SetColor(TRIANGLE_RED, TRIANGLE_BLUE, TRIANGLE_GREEN, TRIANGLE_OPACITY);
-    
-    glUseProgram(g_program.programID);
-    
-    glClearColor(BG_RED, BG_BLUE, BG_GREEN, BG_OPACITY);
+
+    program.SetColor(TRIANGLE_RED, TRIANGLE_BLUE, TRIANGLE_GREEN,
+                     TRIANGLE_OPACITY);
+
+    glUseProgram(program.programID);
+
+    glClearColor(BG_RED, BG_GREEN, BG_BLUE, BG_OPACITY);
 }
 
-void process_input()
-{
+void process_input() {
     SDL_Event event;
-    while (SDL_PollEvent(&event))
-    {
-        if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE)
-        {
+    while (SDL_PollEvent(&event)) {
+        if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE) {
             g_game_is_running = false;
         }
     }
 }
 
-void update()
-{
+void update() {
     // float ticks = (float) SDL_GetTicks() / MILLISECONDS_IN_SECOND; // get the current number of ticks
     // float delta_time = ticks - g_previous_ticks; // the delta time is the difference from the last frame
     // g_previous_ticks = ticks;
 
     // g_triangle_x += 1.0f * delta_time;
     // g_triangle_rotate += DEGREES_PER_SECOND * delta_time; // 90-degrees per second
-    g_model_matrix = glm::mat4(1.0f);
-    
+
 
     // /* Translate -> Rotate */
     // g_model_matrix = glm::translate(g_model_matrix, glm::vec3(g_triangle_x, 0.0f, 0.0f));
@@ -116,30 +114,29 @@ void update()
 void render_rectangle(float half_width, float half_height, glm::vec3 position) {
     constexpr auto base_matrix = glm::mat4(1.0f);
     const auto model_matrix = glm::translate(base_matrix, position);
-    g_program.SetModelMatrix(model_matrix);
+    program.SetModelMatrix(model_matrix);
     float vertices[] =
-    {
-        -half_width , -half_height, 
-         half_width, -half_height, 
-         half_width,  half_height,
-        -half_width , -half_height, 
-        half_width, half_height,
-        -half_width, half_height,
-    };
-    
-    glVertexAttribPointer(g_program.positionAttribute, 2, GL_FLOAT, false, 0, vertices);
-    glEnableVertexAttribArray(g_program.positionAttribute);
+            {
+                    -half_width, -half_height,
+                    half_width, -half_height,
+                    half_width, half_height,
+                    -half_width, -half_height,
+                    half_width, half_height,
+                    -half_width, half_height,
+            };
+
+    glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0,
+                          vertices);
+    glEnableVertexAttribArray(program.positionAttribute);
     glDrawArrays(GL_TRIANGLES, 0, 6);
-    glDisableVertexAttribArray(g_program.positionAttribute);
-    
+    glDisableVertexAttribArray(program.positionAttribute);
+
 }
 
 void render() {
     glClear(GL_COLOR_BUFFER_BIT);
-    render_rectangle(1, 2, glm::vec3(1, -1, 0));    
-    render_rectangle(1, 1, glm::vec3(-1, -1, 0));    
-   
-    
+    render_rectangle(1, 2, glm::vec3(1, -1, 0));
+    render_rectangle(1, 1, glm::vec3(-1, -1, 0));
     SDL_GL_SwapWindow(g_display_window);
 }
 
@@ -148,17 +145,15 @@ void shutdown() { SDL_Quit(); }
 /**
  Start here—we can see the general structure of a game loop without worrying too much about the details yet.
  */
-int main(int argc, char* argv[])
-{
+int main() {
     initialise();
-    
-    while (g_game_is_running)
-    {
+
+    while (g_game_is_running) {
         process_input();
         update();
         render();
     }
-    
+
     shutdown();
     return 0;
 }

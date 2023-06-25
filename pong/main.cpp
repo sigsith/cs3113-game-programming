78aebc45100f074da36c3562ad52f24ec115a7dd
yifei-yao <yaoyifei0@gmail.com>
2023-06-25 15:06:09 -0400

    Import better starting code

--- pong/main.cpp
+++ pong/main.cpp
@@ -1,5 +1,6 @@
 #define GL_SILENCE_DEPRECATION
 #define GL_GLEXT_PROTOTYPES 1
+#define LOG(argument) std::cout << argument << '\n'
 
 #ifdef _WINDOWS
 #include <GL/glew.h>
@@ -7,66 +8,56 @@
 
 #include <SDL.h>
 #include <SDL_opengl.h>
-#include "glm/mat4x4.hpp"                // 4x4 Matrix
-#include "glm/gtc/matrix_transform.hpp"  // Matrix transformation methods
-#include "ShaderProgram.h"               // We'll talk about these later in the course
+#include "glm/mat4x4.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "ShaderProgram.h"
 
-// Our window dimensions
 const int WINDOW_WIDTH  = 640,
           WINDOW_HEIGHT = 480;
 
-// Heartbeat stuff
-const float GROWTH_FACTOR = 1.01f;  // grow by 1.0% / frame
-const float SHRINK_FACTOR = 0.99f;  // grow by -1.0% / frame
-const int MAX_FRAMES = 40;
-
-// Rotation stuff
-const float ROT_ANGLE = glm::radians(1.0f);
-
-// Translation stuff
-const float TRAN_VALUE = 0.025f;
-
-int g_frame_counter = 0;
-bool g_is_growing = true;
-
-// Background color components
 const float BG_RED     = 0.1922f,
             BG_BLUE    = 0.549f,
             BG_GREEN   = 0.9059f,
             BG_OPACITY = 1.0f;
 
-// Our viewport—or our "camera"'s—position and dimensions
 const int VIEWPORT_X      = 0,
           VIEWPORT_Y      = 0,
           VIEWPORT_WIDTH  = WINDOW_WIDTH,
           VIEWPORT_HEIGHT = WINDOW_HEIGHT;
 
-// Our shader filepaths; these are necessary for a number of things
-// Not least, to actually draw our shapes 
-// We'll have a whole lecture on these later
 const char V_SHADER_PATH[] = "shaders/vertex.glsl",
            F_SHADER_PATH[] = "shaders/fragment.glsl";
 
-// Our object's fill colour
-const float TRIANGLE_RED     = 1.0,
-            TRIANGLE_BLUE    = 0.4,
-            TRIANGLE_GREEN   = 0.4,
-            TRIANGLE_OPACITY = 1.0;
+const int TRIANGLE_RED     = 1.0,
+          TRIANGLE_BLUE    = 0.4,
+          TRIANGLE_GREEN   = 0.4,
+          TRIANGLE_OPACITY = 1.0;
+
+const float MILLISECONDS_IN_SECOND = 1000.0;
+const float DEGREES_PER_SECOND     = 90.0f;
+
+SDL_Window* g_display_window;
 
 bool g_game_is_running = true;
-SDL_Window* g_display_window;
+bool g_is_growing      = true;
+
 ShaderProgram g_program;
-glm::mat4 g_view_matrix,        // Defines the position (location and orientation) of the camera
-          g_model_matrix,       // Defines every translation, rotation, and/or scaling applied to an object; we'll look at these next week
-          g_projection_matrix;  // Defines the characteristics of your camera, such as clip panes, field of view, projection method, etc.
+glm::mat4 g_view_matrix,
+          g_model_matrix,
+          g_projection_matrix,
+          g_trans_matrix;
+
+float g_triangle_x      = 0.0f;
+float g_triangle_rotate = 0.0f;
+float g_previous_ticks  = 0.0f;
 
 void initialise()
 {
     SDL_Init(SDL_INIT_VIDEO);
-    g_display_window = SDL_CreateWindow("Hello, Triangle!",
-                                      SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-                                      WINDOW_WIDTH, WINDOW_HEIGHT,
-                                      SDL_WINDOW_OPENGL);
+    g_display_window = SDL_CreateWindow("Hello, Delta Time!",
+                                        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                                        WINDOW_WIDTH, WINDOW_HEIGHT,
+                                        SDL_WINDOW_OPENGL);
     
     SDL_GLContext context = SDL_GL_CreateContext(g_display_window);
     SDL_GL_MakeCurrent(g_display_window, context);
@@ -75,18 +66,13 @@ void initialise()
     glewInit();
 #endif
     
-    // Initialise our camera
     glViewport(VIEWPORT_X, VIEWPORT_Y, VIEWPORT_WIDTH, VIEWPORT_HEIGHT);
     
-    // Load up our shaders
     g_program.Load(V_SHADER_PATH, F_SHADER_PATH);
     
-    // Initialise our view, model, and projection matrices
-    g_view_matrix       = glm::mat4(1.0f);  // Defines the position (location and orientation) of the camera
-    g_model_matrix      = glm::mat4(1.0f);  // Defines every translation, rotations, or scaling applied to an object
+    g_view_matrix = glm::mat4(1.0f);  // Defines the position (location and orientation) of the camera
     g_projection_matrix = glm::ortho(-5.0f, 5.0f, -3.75f, 3.75f, -1.0f, 1.0f);  // Defines the characteristics of your camera, such as clip planes, field of view, projection method etc.
-    
-//    g_model_matrix = glm::translate(g_model_matrix, glm::vec3(5.0f, 0.0f, 0.0f));
+    g_trans_matrix = g_model_matrix;
     
     g_program.SetProjectionMatrix(g_projection_matrix);
     g_program.SetViewMatrix(g_view_matrix);
@@ -94,7 +80,6 @@ void initialise()
     
     g_program.SetColor(TRIANGLE_RED, TRIANGLE_BLUE, TRIANGLE_GREEN, TRIANGLE_OPACITY);
     
-    // Each object has its own unique ID
     glUseProgram(g_program.programID);
     
     glClearColor(BG_RED, BG_BLUE, BG_GREEN, BG_OPACITY);
@@ -114,22 +99,18 @@ void process_input()
 
 void update()
 {
-    glm::vec3 scale_vector;
-    g_frame_counter += 1;
+    float ticks = (float) SDL_GetTicks() / MILLISECONDS_IN_SECOND; // get the current number of ticks
+    float delta_time = ticks - g_previous_ticks; // the delta time is the difference from the last frame
+    g_previous_ticks = ticks;
+
+    g_triangle_x += 1.0f * delta_time;
+    g_triangle_rotate += DEGREES_PER_SECOND * delta_time; // 90-degrees per second
+    g_model_matrix = glm::mat4(1.0f);
     
-    if (g_frame_counter >= MAX_FRAMES)
-    {
-        g_is_growing = !g_is_growing;
-        g_frame_counter = 0;
-    }
-    
-    scale_vector = glm::vec3(g_is_growing ? GROWTH_FACTOR : SHRINK_FACTOR,
-                             g_is_growing ? GROWTH_FACTOR : SHRINK_FACTOR,
-                             1.0f);
-    
-    g_model_matrix = glm::translate(g_model_matrix, glm::vec3(TRAN_VALUE, 0.0f, 0.0f));
-    g_model_matrix = glm::rotate(g_model_matrix, ROT_ANGLE, glm::vec3(0.0f, 1.0f, 0.0f));
-    g_model_matrix = glm::scale(g_model_matrix, scale_vector);
+
+    /* Translate -> Rotate */
+    g_model_matrix = glm::translate(g_model_matrix, glm::vec3(g_triangle_x, 0.0f, 0.0f));
+    g_model_matrix = glm::rotate(g_model_matrix, glm::radians(g_triangle_rotate), glm::vec3(0.0f, 0.0f, 1.0f));
 }
 
 void render() {
@@ -139,7 +120,7 @@ void render() {
     
     float vertices[] =
     {
-         1.0f, -0.5f,
+         0.5f, -0.5f,
          0.0f,  0.5f,
         -0.5f, -0.5f
     };
@@ -159,16 +140,15 @@ void shutdown() { SDL_Quit(); }
  */
 int main(int argc, char* argv[])
 {
-    // Initialise our program—whatever that means
     initialise();
     
     while (g_game_is_running)
     {
-        process_input();  // If the player did anything—press a button, move the joystick—process it
-        update();         // Using the game's previous state, and whatever new input we have, update the game's state
-        render();         // Once updated, render those changes onto the screen
+        process_input();
+        update();
+        render();
     }
     
-    shutdown();  // The game is over, so let's perform any shutdown protocols
+    shutdown();
     return 0;
 }

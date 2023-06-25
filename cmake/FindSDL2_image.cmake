e5c831d37d69a0ae0148046a6a94a347e3d41fa9
yifei-yao <yaoyifei0@gmail.com>
2023-06-25 15:01:53 -0400

    Remove garbage

--- cmake/FindSDL2_image.cmake
+++
@@ -1,7 +0,0 @@
-# FindSDL2_image.cmake
-find_path(SDL2_IMAGE_INCLUDE_DIR NAMES SDL_image.h PATH_SUFFIXES SDL2)
-
-find_library(SDL2_IMAGE_LIBRARY NAMES SDL2_image)
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(SDL2_image REQUIRED_VARS SDL2_IMAGE_LIBRARY SDL2_IMAGE_INCLUDE_DIR)

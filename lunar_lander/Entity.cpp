4c6a6f40a203555ce869578f383a36c224854a35
yifei-yao <yaoyifei0@gmail.com>
2023-07-05 12:20:32 -0400

    Initial ship definition

--- lunar_lander/Entity.cpp
+++ lunar_lander/Entity.cpp
@@ -9,3 +9,41 @@
 **/
 
 #include "Entity.h"
+
+namespace ship {
+    void Ship::Update(float delta_time) {
+        _position += _velocity * delta_time;
+        _velocity += _acceleration * delta_time;
+        _acceleration = _is_engine_on ? GRAVITY +
+                                        VectorByAngle(THRUST, _orientation)
+                                      : GRAVITY;
+        _orientation += _angular_velocity * delta_time;
+        _angular_velocity += _angular_acceleration * delta_time;
+        _angular_acceleration = static_cast<float>(_rcs_state) * RCS_THRUST;
+    }
+
+    void Ship::Render(ShaderProgram &program) {
+        // TODO
+    }
+
+    Ship::Ship(glm::vec3 position, glm::vec3 velocity, float orientation) :
+            _position(position), _velocity(velocity),
+            _orientation(orientation) {
+
+    }
+
+    void Ship::SetEngine(bool is_on) {
+        _is_engine_on = is_on;
+    }
+
+    void Ship::SetRcs(int state) {
+        _rcs_state = state;
+    }
+
+    glm::vec3 VectorByAngle(float scalar, float angle_in_degree) {
+        float angle_in_radians = glm::radians(angle_in_degree);
+        float x = scalar * glm::cos(angle_in_radians);
+        float y = scalar * glm::sin(angle_in_radians);
+        return {x, y, 0.0f};
+    }
+}
--- lunar_lander/Entity.h
+++ lunar_lander/Entity.h
@@ -12,17 +12,63 @@
 #define LUNAR_LANDER_ENTITY_H
 
 #include "ShaderProgram.h"
+#include "glm/mat4x4.hpp"
+#include "glm/gtc/matrix_transform.hpp"
 
 class Entity {
-private:
-
 public:
-    Entity();
+    // The value should be set to a small fix number for all instances.
+    virtual void Update(float delta_time) = 0;
 
-    void Update(float delta_time);
-
-    void Render(ShaderProgram *program);
+    virtual void Render(ShaderProgram &program) = 0;
 };
 
+namespace ship {
+// Model of movement:
+// Three keys to control the movement of a lunar lander:
+// S: when pressed, engine turns on; not pressed, turns off. (It's a very good engine).
+// A: when pressed, RCS is turned on and rotate counter-clockwise; not pressed, turns off.
+// D: when pressed, RCS is turned on and rotate clockwise; not pressed, turns off.
+// When engine is on, because it's such a good engine, the acceleration discretely
+// changed to the combination of gravity and a constant thrust (towards the direction
+// of the orientation), and when it's off, switched to gravity only immediately.
+// For RCS, when pressed a constant angular acceleration is applied towards the
+// intended direction, when not pressed, removed immediately. Also, when both A
+// and D are pressed, nothing happens.
+
+// This value is used for acceleration when no other forces are around.
+    constexpr glm::vec3 GRAVITY = glm::vec3(0.0f, -0.1f, 0.0f);
+// THRUST and RCS_THRUST here are acceleration and angular acceleration
+    constexpr float THRUST = 0.1f;
+    constexpr float RCS_THRUST = 1.0f;
+
+    class Ship : public Entity {
+    private:
+        glm::vec3 _position;
+        glm::vec3 _velocity;
+        glm::vec3 _acceleration = GRAVITY;
+
+        // Orientation in DEGREE in the Cartesian convention. Always modular by 360.
+        float _orientation;
+        float _angular_velocity = 0; // In degree / second.
+        float _angular_acceleration = 0; // In degree / second^2;
+
+        int _rcs_state = 0;  // -1, 0, 1
+        bool _is_engine_on = false;
+    public:
+        // Assuming the ship is drifting in idle initially.
+        Ship(glm::vec3 position, glm::vec3 velocity, float orientation);
+
+        void SetEngine(bool is_on);
+
+        void SetRcs(int state);
+
+        void Update(float delta_time) override;
+
+        void Render(ShaderProgram &program) override;
+    };
+
+    glm::vec3 VectorByAngle(float scalar, float angle_in_degree);
+}
 
 #endif //LUNAR_LANDER_ENTITY_H

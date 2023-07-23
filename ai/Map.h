05a20dcb01ddd907577d0eef49b56455dc368859
yifei-yao <yaoyifei0@gmail.com>
2023-07-22 23:00:44 -0400

    Add pledges

---
+++ ai/Map.h
@@ -0,0 +1,9 @@
+/**
+* Author: Yifei Yao
+* Assignment: Rise of the AI
+* Date due: 2023-07-21, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/

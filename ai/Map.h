26419c53b959a49b9d3d1af19ecc10c2f4c8b8b6
yifei-yao <yaoyifei0@gmail.com>
2023-07-23 00:50:51 -0400

    Define entity

--- ai/Map.h
+++ ai/Map.h
@@ -24,60 +24,7 @@
 #include "glm/gtc/matrix_transform.hpp"
 #include "ShaderProgram.h"
 
-class Map {
- private:
-  int m_width;
-  int m_height;
+#include "Entity.h"
 
-  // Here, the level_data is the numerical "drawing" of the map
-  unsigned int *m_level_data;
-  GLuint m_texture_id;
 
-  float m_tile_size;
-  int m_tile_count_x;
-  int m_tile_count_y;
-
-  // Just like with rendering text, we're rendering several sprites at once
-  // So we need vectors to store their respective vertices and texture coordinates
-  std::vector<float> m_vertices;
-  std::vector<float> m_texture_coordinates;
-
-  // The boundaries of the map
-  float m_left_bound{}, m_right_bound{}, m_top_bound{}, m_bottom_bound{};
-
- public:
-  // Constructor
-  Map(int width,
-      int height,
-      unsigned int *level_data,
-      GLuint texture_id,
-      float tile_size,
-      int
-      tile_count_x,
-      int tile_count_y);
-
-  // Methods
-  void build();
-  void render(ShaderProgram *program);
-  bool is_solid(glm::vec3 position, float *penetration_x, float *penetration_y);
-
-  // Getters
-  int get_width() const { return m_width; }
-  int get_height() const { return m_height; }
-
-  unsigned int *get_level_data() const { return m_level_data; }
-  GLuint get_texture_id() const { return m_texture_id; }
-
-  float get_tile_size() const { return m_tile_size; }
-  int get_tile_count_x() const { return m_tile_count_x; }
-  int get_tile_count_y() const { return m_tile_count_y; }
-
-  std::vector<float> get_vertices() const { return m_vertices; }
-  std::vector<float> get_texture_coordinates() const { return m_texture_coordinates; }
-
-  float get_left_bound() const { return m_left_bound; }
-  float get_right_bound() const { return m_right_bound; }
-  float get_top_bound() const { return m_top_bound; }
-  float get_bottom_bound() const { return m_bottom_bound; }
-};
 #endif //CS3113_GAME_PROGRAMMING_AI_MAP_H_

c3f3e3e3ea53e407503ca57e1dc31046dfb9dc68
yifei-yao <yaoyifei0@gmail.com>
2023-07-23 09:56:04 -0400

    Fix issues

--- ai/Map.cpp
+++ ai/Map.cpp
@@ -90,7 +90,7 @@ void Map::Render(ShaderProgram *shader) const {
 }
 bool Map::IsSolid(Box &box) const {
   // 1. Check if box in range
-  if (!(box.XMax() < min_x_ || box.XMin() > max_x_) || box.YMax() < min_y_
+  if ((box.XMax() < min_x_ || box.XMin() > max_x_) || box.YMax() < min_y_
       || box.YMin() > max_y_) {
     return false;
   }

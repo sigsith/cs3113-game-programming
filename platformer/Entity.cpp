b36ac39a4c1136061fcac4e2d1b3e7ce3428b3a0
yifei-yao <yaoyifei0@gmail.com>
2023-07-31 15:49:27 -0400

    Update pledges

--- platformer/Entity.cpp
+++ platformer/Entity.cpp
@@ -1,7 +1,7 @@
 /**
 * Author: Yifei Yao
-* Assignment: Rise of the AI
-* Date due: 2023-07-21, 11:59pm
+* Assignment: Platformer
+* Date due: 2023-08-01, 11:59pm
 * I pledge that I have completed this assignment without
 * collaborating with anyone else, in conformance with the
 * NYU School of Engineering Policies and Procedures on

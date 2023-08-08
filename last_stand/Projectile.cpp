fe368025ad779cddffbff58f0073a4ccd5a0f03f
yifei-yao <yaoyifei0@gmail.com>
2023-08-08 05:54:46 -0400

    Add projectile

---
+++ last_stand/Projectile.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Yifei Yao on 8/8/23.
+//
+
+#include "Projectile.h"
---
+++ last_stand/Projectile.h
@@ -0,0 +1,16 @@
+//
+// Created by Yifei Yao on 8/8/23.
+//
+
+#ifndef CS3113_GAME_PROGRAMMING_LAST_STAND_PROJECTILE_H_
+#define CS3113_GAME_PROGRAMMING_LAST_STAND_PROJECTILE_H_
+
+#include "TextureObject.h"
+class Projectile {
+ private:
+  TextureObject text_obj_;
+
+ public:
+};
+
+#endif //CS3113_GAME_PROGRAMMING_LAST_STAND_PROJECTILE_H_
